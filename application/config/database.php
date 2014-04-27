6947f08c7b83267aa3a53fa7bbe82f8d14b45afa
RedBeanPieLarry <RedBeanPieLarry@gmail.com>
2014-04-27 15:27:19 +0800

    tutorial: news section

--- application/config/database.php
+++ application/config/database.php
@@ -45,7 +45,7 @@
 | the active record class
 */
 
-$active_group = 'default';
+$active_group = 'test';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
@@ -63,6 +63,22 @@ $db['default']['dbcollat'] = 'utf8_general_ci';
 $db['default']['swap_pre'] = '';
 $db['default']['autoinit'] = TRUE;
 $db['default']['stricton'] = FALSE;
+
+$db['test']['hostname'] = 'localhost';
+$db['test']['username'] = 'root';
+$db['test']['password'] = 'root';
+$db['test']['database'] = 'codeigniter_db';
+$db['test']['dbdriver'] = 'mysql';
+$db['test']['dbprefix'] = '';
+$db['test']['pconnect'] = TRUE;
+$db['test']['db_debug'] = TRUE;
+$db['test']['cache_on'] = FALSE;
+$db['test']['cachedir'] = '';
+$db['test']['char_set'] = 'utf8';
+$db['test']['dbcollat'] = 'utf8_general_ci';
+$db['test']['swap_pre'] = '';
+$db['test']['autoinit'] = TRUE;
+$db['test']['stricton'] = FALSE;
 
 
 /* End of file database.php */
